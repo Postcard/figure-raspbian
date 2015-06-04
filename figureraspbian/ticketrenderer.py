b92c81462fedfa7aec2f4e0fdaedad813906bc63
benoitguigal <benoit.guigal@gmail.com>
2015-06-04 10:56:03 +0200

    set panpan as random snapshot when first ticket of installation is printed

--- figureraspbian/ticketrenderer.py
+++ figureraspbian/ticketrenderer.py
@@ -51,9 +51,10 @@ def random_selection(variable):
 def render(html, snapshot, random_snapshot, code, date, images, random_text_selections, random_image_selections):
     snapshot_url = 'http://localhost:8080/media/snapshots/%s' % os.path.basename(snapshot)
     context = {'snapshot': snapshot_url}
-    if random_snapshot:
-        random_snapshot_url = 'http://localhost:8080/media/snapshots/%s' % os.path.basename(random_snapshot)
-        context['random_snapshot'] = random_snapshot_url
+    random_snapshot_url = ('http://localhost:8080/media/snapshots/%s' % os.path.basename(random_snapshot) if
+                           random_snapshot else
+                           'http://localhost:8080/media/snapshots/example.jpg')
+    context['random_snapshot'] = random_snapshot_url
     for (text_variable_id, item) in random_text_selections:
         text = item['text'] if item else ''
         context['textvariable_%s' % text_variable_id] = text
