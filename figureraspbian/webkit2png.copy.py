b330e0757fd87db084bdb65d09ccc63b79206b5b
benoitguigal <benoit.guigal@gmail.com>
2017-05-17 14:49:08 +0200

    try launching get_screenshot as multiprocessing.Process

---
+++ figureraspbian/webkit2png.copy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+from os.path import join
+
+import subprocess
+
+from figureraspbian import settings
+from figureraspbian.utils import timeit
+
+@timeit
+def get_screenshot(html):
+    file_path = join(settings.RAMDISK_ROOT, 'ticket.html')
+    with open(file_path, 'wb') as f:
+        f.write(html.encode('utf-8'))
+    file_url = 'file://%s' % file_path
+    output_path = join(settings.RAMDISK_ROOT, 'ticket.png')
+    args = ['webkit2png', '--output', output_path, file_url]
+    subprocess.check_output(args)
+    with open(output_path, 'rb') as ticket:
+        return ticket.read()
+
+
